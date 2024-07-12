493ed0e97a74927529ea18251856202f79b334c6
SaiNarendra079 <158446698+SaiNarendra079@users.noreply.github.com>
2024-07-12 17:59:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+//Data Types - string , boolean,null,undefined,number
+//number
+let a=5
+console.log(a,typeof a)
+//string
+let b='sai'
+console.log(b,typeof b)
+let c= a+b
+console.log(c, typeof c)
+//Boolean
+let x= 1==1
+console.log(x,typeof x)
+
+//undefined
+let y
+console.log(y,typeof y)
+//null
+let z= null
+console.log(z,typeof z)
+
+
+//mathematical Operators(basic arthematic operations)
+let A=3
+let B=25
+//addition
+console.log( 'Addition',A+B)
+//subtraction
+console.log("Subtraction:",A-B)
+//Multiplication
+console.log("Multiplication:",A*B)
+//modulus
+console.log("Modular:",B%A)
+//division
+console.log("Division:",A/B)
+
+//Comparator operators
+
+let K=1
+let L='1'
+let M= '2'
+let N=1
+let P=2
+//K equals to L returns true if equal otherwise return false?
+console.log(K==L)
+//K equals to N returns true if equal otherwise return false?
+console.log(K==N)
+//K equals to M returns true if equal otherwise return false?
+console.log(K==M)
+console.log(K===L)
+console.log(K===M)
+console.log(K===N)
+//K not equals to L?
+console.log(K!=L)
+console.log(K!=N)
+console.log(K!==L)
+console.log(K!==N)
+//greater than & less than
+console.log(K>N)
+console.log(K<P)
+console.log(K>=N)
+console.log(K<=P)
